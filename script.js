ee3553fb170bd9c9402ee84202fcd4fa548aa678
Ronaldo Lima <ronaldoprogramador1997@gmail.com>
2023-11-15 23:12:57 -0300

    Modificações na tela 2 com o quiz

--- script.js
+++ script.js
@@ -1,65 +1,22 @@
-const question = document.querySelector(".question");
-const answers = document.querySelector(".answers");
-const spnQtd = document.querySelector(".spnQtd");
-const textFinish = document.querySelector(".finish span");
-const content = document.querySelector(".content");
-const contentFinish = document.querySelector(".finish");
-const btnRestart = document.querySelector(".finish button");
+const form = document.getElementById('quiz-form');
+let currentQuestion = 0;
 
-import questions from "./questions.js";
-
-let currentIndex = 0;
-let questionsCorrect = 0;
-
-btnRestart.onclick = () => {
-  content.style.display = "flex";
-  contentFinish.style.display = "none";
-
-  currentIndex = 0;
-  questionsCorrect = 0;
-  loadQuestion();
-};
-
-function nextQuestion(e) {
-  if (e.target.getAttribute("data-correct") === "true") {
-    questionsCorrect++;
-  }
-
-  if (currentIndex < questions.length - 1) {
-    currentIndex++;
-    loadQuestion();
-  } else {
-    finish();
-  }
+function showNext() {
+    const questions = document.querySelectorAll('.question');
+    questions[currentQuestion].style.display = 'none';
+    currentQuestion = (currentQuestion + 1) % questions.length;
+    questions[currentQuestion].style.display = 'block';
 }
 
-function finish() {
-  textFinish.innerHTML = `você acertou ${questionsCorrect} de ${questions.length}`;
-  content.style.display = "none";
-  contentFinish.style.display = "flex";
+function showPrevious() {
+    const questions = document.querySelectorAll('.question');
+    questions[currentQuestion].style.display = 'none';
+    currentQuestion = (currentQuestion - 1 + questions.length) % questions.length;
+    questions[currentQuestion].style.display = 'block';
 }
 
-function loadQuestion() {
-  spnQtd.innerHTML = `${currentIndex + 1}/${questions.length}`;
-  const item = questions[currentIndex];
-  answers.innerHTML = "";
-  question.innerHTML = item.question;
-
-  item.answers.forEach((answer) => {
-    const div = document.createElement("div");
-
-    div.innerHTML = `
-    <button class="answer" data-correct="${answer.correct}">
-      ${answer.option}
-    </button>
-    `;
-
-    answers.appendChild(div);
-  });
-
-  document.querySelectorAll(".answer").forEach((item) => {
-    item.addEventListener("click", nextQuestion);
-  });
-}
-
-loadQuestion();
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Adicione aqui a lógica para processar/enviar os dados do formulário
+    // Você pode enviar os dados para um servidor ou realizar outras ações conforme necessário
+});
